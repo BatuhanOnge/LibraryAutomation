4fe41e53f29582c709b22c79b096371b8d614c02
BatuhanOnge <himmet.b.onge@gmail.com>
2021-08-13 11:38:59 +0300

    library

---
+++ Kutuphane/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kutuphane.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Kitaplar()
+        {
+            return View();
+        }
+
+        public ActionResult Hareketler()
+        {
+            return View();
+        }
+        public ActionResult Uyeler()
+        {
+            return View();
+        }
+    }
+}
