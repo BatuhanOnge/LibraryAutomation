ea3c44fd4edc3e9956933115db301dae06e1d8ac
BatuhanOnge <himmet.b.onge@gmail.com>
2021-08-16 14:37:29 +0300

    fix

--- Kutuphane/App_Start/BundleConfig.cs
+++ Kutuphane/App_Start/BundleConfig.cs
@@ -19,10 +19,12 @@ namespace Kutuphane
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                       "~/Scripts/bootstrap.js",
+                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+
+            bundles.Add(new Bundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
