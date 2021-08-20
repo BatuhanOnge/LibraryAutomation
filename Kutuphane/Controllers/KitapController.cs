45e8fafdfa3eb2dcc7979e21a18c3f717ea24374
BatuhanOnge <himmet.b.onge@gmail.com>
2021-08-20 16:33:12 +0300

    kitap

--- Kutuphane/Controllers/KitapController.cs
+++ Kutuphane/Controllers/KitapController.cs
@@ -9,10 +9,11 @@ namespace Kutuphane.Controllers
 {
     public class KitapController : Controller
     {
+        Model1 db = new Model1();
         // GET: Uye
         public ActionResult Index()
         {
-            Model1 db = new Model1();
+            
             try
             {
                 var result = db.KRT_KITAP.ToList();
@@ -26,6 +27,34 @@ namespace Kutuphane.Controllers
             //{
             //    db.Dispose;
             //}
+        }
+
+        [HttpGet]
+        public ActionResult KitapEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KitapEkle(KRT_KITAP newMemberData)
+        {
+            db.KRT_KITAP.Add(newMemberData);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var ISBN = db.KRT_KITAP.Find(id);
+            db.KRT_KITAP.Remove(ISBN);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KitapGuncelle(int id)
+        {
+            var uyeID = db.KRT_KITAP.Find(id);
+            return View("KitapGuncelle", uyeID);
         }
     }
 }
